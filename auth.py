70f5610c29e474b67481e297ce89a42d60ebfb2f
Omri Webber <omriwebber@gmail.com>
2022-11-04 18:45:43 +1300

    fixed auth functions

--- auth.py
+++ auth.py
@@ -14,38 +14,52 @@ def register():
     if request.method == 'POST':
 
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         
+        # query
+        user = Users.query.filter_by(username=username).first()
+        
+        if user:
+            msg = 'Username already exists!'
+            return render_template('auth/register.html',  msg=msg, name='Cooking App')
+
         # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-        new_user = Users(username=username, password=password)
+        new_user = Users(username=username, email=email, password=generate_password_hash(password, method='sha256'))
         
         # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
         
-        flash('Thank you for registering your account, please login!')
         return redirect(url_for('auth.login'))
-        
-    return render_template('auth/register.html',  msg=msg, name='Cooking App - Register')
+    return render_template('auth/register.html',  msg=msg, name='Cooking App')
 
 
 @auth.route('/login', methods=('GET', 'POST'))
 def login():
     msg = ''
-    if request.method == 'POST' and 'password' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
         password = request.form['password']
         
-        user = Users.query.filter_by(password=password).first()
+        user = Users.query.filter_by(username=username).first()
         
         if user is None:
-            msg = "User with that password dosnt exist."
-        else:
+            msg = 'Username dosnt exist. Please try again '
+        elif not check_password_hash(user.password, password):
+            msg = 'Incorrect password.'
+        elif user:
+            # Create session data, we can access this data in other routes
             login_user(user, remember=True)
+            # Redirect to home page
             return redirect(url_for('index'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
             
         print(msg)
 
-    return render_template('auth/login.html', msg=msg, name='Cooking App - Login')
+    return render_template('auth/login.html', msg=msg, name='Cooking App')
    
 
 @auth.route('/logout')
