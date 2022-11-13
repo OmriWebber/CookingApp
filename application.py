2a859b443d6efe0d5d15095582bd2026ca00706a
Omri Webber <omriwebber@gmail.com>
2022-11-14 08:49:25 +1300

    changes for deployment

--- application.py
+++ application.py
@@ -23,6 +23,7 @@ ckeditor = CKEditor(application)
 if 'RDS_DB_NAME' in os.environ:
     RDS_Connection_String = 'mysql://' + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] + '@' + os.environ['RDS_HOSTNAME'] + ':' + os.environ['RDS_PORT'] + '/' + os.environ['RDS_DB_NAME']
     print(RDS_Connection_String)
+    print('TESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSETTESTSETSETSETSETSETSETSETSETSET')
     application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:password@cooking-app-database.ch0or1bnad8y.ap-southeast-2.rds.amazonaws.com:3306/cooking_app_db'
 else:
     application.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost:3306/cookingapp"
